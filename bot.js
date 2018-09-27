93ce0f56de4b61732a267263982d6a23b175d09f
Dontwait00 <35642213+Dontwait00@users.noreply.github.com>
2018-09-27 16:52:06 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,14 +6,13 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping!') {
-       message.reply('ping?');
-        message.edit('pong! Need help? ask here "=help"');
+    if (message.content === '?help') {
+       message.reply('Need help?');
        }
 });
 client.on('message', message => {
-    if (message.content === 'server') {
-       message.reply('pong');
+    if (message.content === '?server') {
+       message.channel.send(`Server infos\nServer name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
        }
 });
 
