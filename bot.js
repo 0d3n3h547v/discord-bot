2d6bebf40b526113f9060ba6925f406332a68fca
Dontwait00 <35642213+Dontwait00@users.noreply.github.com>
2018-09-27 22:12:38 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -57,19 +57,6 @@ client.on('message', message => {
     if (message.content === '?help') {
        message.reply("need help?\nHeres the known commands:\n\n`?server`\n`?user`\n`?ping`");
        }
-    });
-client.on('message', message => {
-    if (message.content === '?ping') {
-       message.channel.send(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
-       }
-});
-client.on('message', message => {
-    if (message.content === '?server') {
-       message.channel.send(`:satellite_orbital:\n$GETTING SERVER INFORMATION:\n\nName: **${message.guild.name}**\nMembers: **${message.guild.memberCount}**\nServer created: **${message.guild.createdAt}**\nRegion: **${message.guild.region}**\n\n$end transmission\n:satellite:`);
-       }
-    else if (message.content === `?user`) {
-    message.channel.send(`your username: **${message.author.username}**\nYour ID: **${message.author.id}**`);
-       }
-    }
+     }
   });
 client.login(process.env.BOT_TOKEN);
