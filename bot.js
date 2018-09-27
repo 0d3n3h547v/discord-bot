346b025ee990b29c246dc02c7c4b55fe0b572e50
Dontwait00 <35642213+Dontwait00@users.noreply.github.com>
2018-09-27 18:54:55 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,8 +12,7 @@ client.on('message', message => {
     });
 client.on('message', message => {
     if (message.content === '?ping') {
-    const m = await message.channel.send("Ping?");
-    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+       message.reply(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
        }
 });
 client.on('message', message => {
