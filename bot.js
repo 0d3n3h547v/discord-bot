e3500cf7211710a33f9631164ec8c36112925884
Dontwait00 <35642213+Dontwait00@users.noreply.github.com>
2018-09-27 18:38:32 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,6 +17,15 @@ client.on('message', message => {
     else if (message.content === `?user`) {
     message.channel.send(`your username: **${message.author.username}**\nYour ID: **${message.author.id}**`);
        }
+    
+ client.on('message', message => {
+    if(message.content === '?purge') {
+    // This command removes all messages from all users in the channel, from 2 up to 10000.
+    const deleteCount = parseInt(args[0], 1000);
+    if(!deleteCount || deleteCount < 2 || deleteCount > 10000)
+return message.reply("Please provide a number between 2 and 10.000 for the number of messages to delete");
+    }
+ }
 });
 
 client.login(process.env.BOT_TOKEN);
