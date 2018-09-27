5f3d650b30ef9d589105e2f3cd793a9fe80eb238
Dontwait00 <35642213+Dontwait00@users.noreply.github.com>
2018-09-27 19:49:24 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,27 +1,23 @@
 const Discord = require('discord.js');
+
+// Create an instance of a Discord client
 const client = new Discord.Client();
 
+/**
+ * The ready event is vital, it means that only _after_ this will your bot start reacting to information
+ * received from Discord
+ */
 client.on('ready', () => {
-    console.log('I am ready!');
+  console.log('I am ready!');
 });
 
+// Create an event listener for messages
 client.on('message', message => {
-    if (message.content === '?help') {
-       message.reply("need help?\nHeres the known commands:\n\n`?server`\n`?user`\n`?ping`");
-       }
-    });
-client.on('message', message => {
-    if (message.content === '?ping') {
-       message.channel.send(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
-       }
-});
-client.on('message', message => {
-    if (message.content === '?server') {
-       message.channel.send(`:satellite_orbital:\n$GETTING SERVER INFORMATION:\n\nName: **${message.guild.name}**\nMembers: **${message.guild.memberCount}**\nServer created: **${message.guild.createdAt}**\nRegion: **${message.guild.region}**\n\n$end transmission\n:satellite:`);
-       }
-    else if (message.content === `?user`) {
-    message.channel.send(`your username: **${message.author.username}**\nYour ID: **${message.author.id}**`);
-       }
+  // If the message is "ping"
+  if (message.content === 'ping') {
+    // Send "pong" to the same channel
+    message.channel.send('pong');
+  }
 });
 
 client.login(process.env.BOT_TOKEN);
