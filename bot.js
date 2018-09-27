2bb04c7d5bb9565a93a1ce1f033f1e74e79da710
Dontwait00 <35642213+Dontwait00@users.noreply.github.com>
2018-09-27 21:41:09 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,7 +19,7 @@ client.on('message', message => {
   if (message.content.startsWith('!kick')) {
     // Assuming we mention someone in the message, this will return the user
     // Read more about mentions over at https://discord.js.org/#/docs/main/stable/class/MessageMentions
-    if(!message.member.roles.some(r=>["OWNERS", "Moderator"].includes(r.name)) )
+    if(!message.member.roles.some(r=>["OWNERS", "ADMIN"].includes(r.name)) )
     return message.reply("Sorry, you don't have permissions to use this!");
     const user = message.mentions.users.first();
     // If we have a user mentioned
