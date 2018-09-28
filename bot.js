a7ac920ee36bf8c1592ffdea9529f067803771fa
Dontwait00 <35642213+Dontwait00@users.noreply.github.com>
2018-09-28 20:20:56 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,9 +10,15 @@ client.on('ready', () => {
 
 client.on('message', message => {
   if (!message.guild) return;
+      
+      const prefix = '?';
+      
+      let sender = message.author; // This variable takes the message, and finds who the author is.
+    let cont = message.content.slice(prefix.length).split(" "); // This variable slices off the prefix, then puts the rest in an array based off the spaces
+    let args = cont.slice(1); // This slices off the command in cont, only leaving the arguments.
 
 //=================================================================== kick
-  if (message.content.startsWith('?kick')) {
+  if (message.content.startsWith(prefix + 'kick')) {
     if(!message.member.roles.some(r=>["OWNERS", "Admin"].includes(r.name)) )
     return message.reply("what are you trying to do? Only the owners can use this command");
     const user = message.mentions.users.first();
@@ -33,7 +39,7 @@ client.on('message', message => {
     }
   }
 //=================================================================== ban
-    if (message.content.startsWith('?ban')) {
+    if (message.content.startsWith(prefix + 'ban')) {
       if(!message.member.roles.some(r=>["OWNERS"].includes(r.name)) )
       return message.reply("what are you trying to do? Only the owners can use this command");
     const user = message.mentions.users.first();
@@ -56,16 +62,16 @@ client.on('message', message => {
       }
     }
 //===================================================================== help
-    if (message.content === '?server') {
+    if (message.content === prefix + 'server') {
        message.channel.send(`:satellite_orbital:\`\`\`js\n$GETTING SERVER INFORMATION:\n\nName: ${message.guild.name}\nServer ID: ${message.guild.id}\nMembers: ${message.guild.memberCount}\nServer build: ${message.guild.createdAt}\nServer Creator: ${message.guild.owner}\nOwner ID: ${message.guild.ownerID}\nRegion: ${message.guild.region}\nVerification level: ${message.guild.verificationLevel}\nServer verified: ${message.guild.verified}\n\n\n$end transmission\`\`\``);
        }
-    else if (message.content === `?user`) {
+    else if (message.content === prefix + 'user') {
     message.channel.send(`**Your username:** \`${message.author.username}\`\n**Your ID:** \`${message.author.id}\`\n**Created on:** \`${message.author.createdAt}\`\n**Discord tag:** \`${message.author.tag}\`\n`);
        }
-    if (message.content === '?ping') {
+    if (message.content === prefix + 'ping') {
        message.channel.send(`:ping_pong: motherfucker! Latency is **${Math.round(client.ping)}ms**`);
        }
-  if (message.content === '?embed') {
+  if (message.content === prefix + 'embed') {
     // We can create embeds using the MessageEmbed constructor
     // Read more about all that you can do with the constructor
     // over at https://discord.js.org/#/docs/main/stable/class/RichEmbed
@@ -80,7 +86,7 @@ client.on('message', message => {
     message.channel.send(embed);
   }
   
-if (message.content.startsWith('?google')) {
+if (message.content.startsWith(prefix + 'google')) {
 // Depending on your command framework (or if you use one), it doesn't have to
 // edit messages so you can rework it to fit your needs. Again, this doesn't have
 // to be async if you don't care about message editing.
@@ -111,17 +117,37 @@ async function googleCommand(msg, args) {
      });
    }
  }
-      if (message.content.startsWith('?clean')) {
-    const deleteCount = parseInt(args[0], 10);
-  if(!deleteCount || deleteCount < 2 || deleteCount > 100)
-      return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
-            async function purge() {
-            message.delete();
-    const fetched = await message.channel.fetchMessages({limit: deleteCount});
-    message.channel.bulkDelete(fetched)
-      .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
-                    }
-           purge();
-      }
+      if (message.content.startsWith(prefix + 'clean')) { // This time we have to use startsWith, since we will be adding a number to the end of the command.
+        // We have to wrap this in an async since awaits only work in them.
+        async function purge() {
+            message.delete(); // Let's delete the command message, so it doesn't interfere with the messages we are going to delete.
+
+            // Now, we want to check if the user has the `bot-commander` role, you can change this to whatever you want.
+            if (!message.member.roles.find("name", "bot-commander")) { // This checks to see if they DONT have it, the "!" inverts the true/false
+                message.channel.send('You need the \`bot-commander\` role to use this command.'); // This tells the user in chat that they need the role.
+                return; // this returns the code, so the rest doesn't run.
+            }
+
+            // We want to check if the argument is a number
+            if (isNaN(args[0])) {
+                // Sends a message to the channel.
+                message.channel.send('Please use a number as your arguments. \n Usage: ' + prefix + 'clean <amount>'); //\n means new line.
+                // Cancels out of the script, so the rest doesn't run.
+                return;
+            }
+
+            const fetched = await message.channel.fetchMessages({limit: args[0]}); // This grabs the last number(args) of messages in the channel.
+            console.log(fetched.size + ' messages found, deleting...'); // Lets post into console how many messages we are deleting
+
+            // Deleting the messages
+            message.channel.bulkDelete(fetched)
+                .catch(error => message.channel.send(`Error: ${error}`)); // If it finds an error, it posts it into the channel.
+
+        }
+
+        // We want to make sure we call the function whenever the purge command is run.
+        purge(); // Make sure this is inside the if(msg.startsWith)
+
+    }
  });
 client.login(process.env.BOT_TOKEN);
