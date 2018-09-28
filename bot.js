f2c2cc1995c0021cedc05edc301e44058911c57d
Dontwait00 <35642213+Dontwait00@users.noreply.github.com>
2018-09-28 10:57:26 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -53,13 +53,5 @@ client.on('message', message => {
     }
   }
 //===================================================================== new member
-  client.on('guildMemberAdd', member => {
-  // Send the message to a designated channel on a server:
-  const channel = member.guild.channels.find(ch => ch.name === 'welcome-messages');
-  // Do nothing if the channel wasn't found on this server
-  if (!channel) return;
-  // Send the message, mentioning the member
-  channel.send(`Welcome to the server, ${member}`);
-      }
  });
 client.login(process.env.BOT_TOKEN);
