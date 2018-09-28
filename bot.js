4198829a37e5ab1383c5ce6d85714a0ca3e636bf
Dontwait00 <35642213+Dontwait00@users.noreply.github.com>
2018-09-28 13:08:17 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -52,14 +52,12 @@ client.on('message', message => {
       }
     }
 //===================================================================== help
-  ('message', message => {
     if (message.content === '?server') {
        message.channel.send(`:satellite_orbital:\n$GETTING SERVER INFORMATION:\n\nName: **${message.guild.name}**\nMembers: **${message.guild.memberCount}**\nServer created: **${message.guild.createdAt}**\nRegion: **${message.guild.region}**\n\n$end transmission\n:satellite:`);
        }
     else if (message.content === `?user`) {
     message.channel.send(`your username: **${message.author.username}**\nYour ID: **${message.author.id}**`);
        }
-    ('message', message => {
     if (message.content === '?ping') {
        message.channel.send(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
        }
